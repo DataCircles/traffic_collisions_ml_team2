93f4a918e2941fb367e6fbf28aabbeb49aae9e40
wlemmon <lemmon.warren@gmail.com>
2020-07-26 22:00:39 -0700

    updated heatmap code to use cartopy, fixed heatmap transform bug. cleaned up environment.yaml file.

--- heatmap.py
+++ heatmap.py
@@ -4,10 +4,10 @@ from scipy.ndimage.filters import gaussian_filter
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
 from utils import *
-import geopandas as gpd
-from shapely.geometry import Point, Polygon
+import cartopy.crs as ccrs
+from cartopy.io.img_tiles import *
+import percache
 
 # remove na
 coll = open_collisions()
@@ -22,36 +22,60 @@ cmap = {
   'blue': ((0.0, 0.5, 0.5), (0.11, 1, 1), (0.34, 1, 1), (0.65, 0, 0), (1, 0, 0)),
   'alpha': ((0.0, 0.3, 0.3), (0.05, 0.3, 0.3), (0.3, .7, .7), (0.89, .9, .9), (1, 0.9, 0.9)),
 }
-
-def heatmap(x, y, sigma=1, weights=None, bins=5000):
-    hm, xedges, yedges = np.histogram2d(x, y, weights=weights, density=True, bins=bins)
-    hm = gaussian_filter(hm, sigma=sigma)
-    # left, right, bottom, top
-    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
-    return hm.T, extent
-
-fig, ax = plt.subplots(figsize=(5, 10))
-m = Basemap(projection='mill',
-            resolution='c',
-            llcrnrlon=coll.X.min(),
-            llcrnrlat=coll.Y.min(),
-            urcrnrlon=coll.X.max(),
-            urcrnrlat=coll.Y.max(),
-            epsg = 2285,
-            )
-
-m.arcgisimage(service='World_Street_Map', xpixels = 5000, verbose= False)
-
 cm.myjet = matplotlib.colors.LinearSegmentedColormap(name='myjet', segmentdata=cmap, N=256, gamma=2.)
 
-img, extent = heatmap(coll.X, coll.Y, weights=coll.INJURIES, sigma=10.0)
-# noremalize to 0-256
-img = (img - img.min())/(img.max() - img.min())*256
+tiler = OSM()
+geo = ccrs.PlateCarree()
+x, y, z = np.hsplit(tiler.crs.transform_points(geo, coll.X.values, coll.Y.values), 3)
+coll.loc[:,'X'] = x
+coll.loc[:,'Y'] = y
+extent = (coll.X.min(), coll.X.max(), coll.Y.min(), coll.Y.max())
 
-# run each coordinate through the map, then return to extent format
-extent = [i for _l in m(extent[:2], extent[2:]) for i in _l]
-ax.imshow(img, extent=extent, origin='lower', cmap=cm.myjet, )
+w = 4000
+factor = (extent[3]-extent[2])/(extent[1]-extent[0])
+h = int(w*factor)
 
-plt.savefig('heatmap.jpg', dpi=800)
-plt.show()
-asdf
+@cache
+def heatmap(x, y, sigma=1, weights=None, bins=5000):
+    print('creating heatmap')
+    hm, xedges, yedges = np.histogram2d(x, y, weights=weights, density=True, bins=bins)
+    hm = gaussian_filter(hm, sigma=sigma)
+    return hm.T
+
+def heatmap_postprocessing(img):
+    return (img - img.min())/(img.max() - img.min())*256
+
+def heatmap_plot(img, fname):
+    fig, ax = plt.subplots(figsize=(w/1000, h/1000))
+    ax = plt.axes(projection=tiler.crs)
+    
+    ax.set_extent(extent, crs=tiler.crs)
+    
+    composite = get_composite(tiler, extent)
+    ax.imshow(composite[0], extent=composite[1], origin=composite[2])
+    ax.imshow(img, origin='lower', extent=extent, cmap=cm.myjet)#, alpha=.5)
+    plt.savefig(fname, dpi=1000)
+
+@cache
+def get_composite(tiler, extent):
+    z_target = 14
+    composite = tiler.image_for_domain(sgeom.box(extent[0], extent[2], extent[1], extent[3]), target_z=z_target)
+    return composite
+
+img_col = heatmap(coll.X, coll.Y, weights=None, sigma=10.0, bins=[w, h])
+img_col = heatmap_postprocessing(img_col)
+heatmap_plot(img_col, 'heatmap-coll.jpg')
+
+img_inj = heatmap(coll.X, coll.Y, weights=coll.INJURIES, sigma=10.0, bins=[w, h])
+img_inj = heatmap_postprocessing(img_inj)
+heatmap_plot(img_inj, 'heatmap-injuries.jpg')
+
+img_fat = heatmap(coll.X, coll.Y, weights=coll.FATALITIES, sigma=10.0, bins=[w, h])
+img_fat = heatmap_postprocessing(img_fat)
+heatmap_plot(img_fat, 'heatmap-fatalities.jpg')
+
+img_inj_dens = np.where(img_col > 0, img_inj/img_col, 0)
+img_inj_dens = heatmap_postprocessing(img_inj_dens)
+heatmap_plot(img_inj_dens, 'heatmap-inj-dens.jpg')
+
+
--- utils.py
+++ utils.py
@@ -1,32 +1,9 @@
 import os.path
 import pandas as pd
-import pickle
-import geopandas as gpd
 
-def pickleable(func):
+import percache
+cache = percache.Cache("my-cache")
 
-  def inner(*args, **kwargs):
-    pfile = kwargs['file']+str('.pickle')
-    if os.path.exists(pfile):
-      with open(pfile, 'rb') as f:
-        return pickle.load(f)
-    df = func(*args, **kwargs)
-    with open(pfile, 'wb') as f:
-      pickle.dump(df, f)
-    return df
-  return inner
-  
-
-@pickleable
-def _open_collisions(file):
-  return pd.read_csv(file=file, parse_dates=['INCDATE', 'INCDTTM'])
-def open_collisions():
-  return _open_collisions(file='./data/Collisions.csv')
-  
-
-@pickleable
-def _open_streets(file):
- return gpd.read_file(file)
-
-def open_streets():
- return _open_streets(file="./data/Seattle_Streets-shp/Seattle_Streets.shp")
+@cache
+def open_collisions(file='./data/Collisions.csv'):
+  return pd.read_csv(file, parse_dates=['INCDATE', 'INCDTTM'])
